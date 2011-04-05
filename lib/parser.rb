76a4f6f18d1ca61b95b5c98f4bb24170a22091e7
mjy <diapriid@gmail.com>
2011-04-05 12:48:01 -0400

    cosmetic/version bump

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,21 +5,21 @@ class OboParser::Parser
   end
 
   def parse_file
-    # toss everything right now, we just want the terms
+    # At present we ignore the header lines
     while !@lexer.peek(OboParser::Tokens::Term)
       @lexer.pop(OboParser::Tokens::TagValuePair)
     end
 
     i = 0
     while !@lexer.peek(OboParser::Tokens::Typedef) && !@lexer.peek(OboParser::Tokens::EndOfFile)
-      raise OboParser::ParseError, "infinite loop in Terms" if i > 10000000 
+      raise OboParser::ParseError, "infinite loop in Terms" if i > 10000000 # there aren't that many words! 
       parse_term
       i += 1
     end
 
     i = 0
     while @lexer.peek(OboParser::Tokens::Typedef) 
-      raise OboParser::ParseError,"infinite loop in Terms" if i > 1000000 # there aren't that many words!
+      raise OboParser::ParseError,"infinite loop in Typedefs" if i > 1000000 
       parse_typedef
       i += 1
     end    
@@ -29,11 +29,11 @@ class OboParser::Parser
     t = @lexer.pop(OboParser::Tokens::Term)
     tags = []
     while !@lexer.peek(OboParser::Tokens::Term) && !@lexer.peek(OboParser::Tokens::Typedef) && !@lexer.peek(OboParser::Tokens::EndOfFile) 
-      if @lexer.peek(OboParser::Tokens::TagValuePair)
+      begin
         t = @lexer.pop(OboParser::Tokens::TagValuePair)
-        tags.push [t.tag, t.value]
-      else
-        raise(OboParser::ParseError, "Expected a tag-value pair, but did not get one following this tag/value: [#{t.tag} / #{t.value}]")
+        tags.push(t) 
+      rescue 
+        raise 
       end
     end
     @builder.add_term(tags)
@@ -41,11 +41,14 @@ class OboParser::Parser
 
   def parse_typedef
     @lexer.pop(OboParser::Tokens::Typedef)
-    #  @t = @builder.stub_typdef
     tags = []
     while !@lexer.peek(OboParser::Tokens::Typedef) && @lexer.peek(OboParser::Tokens::TagValuePair)
-      t = @lexer.pop(OboParser::Tokens::TagValuePair)
-      tags.push [t.tag, t.value]
+      begin
+        t = @lexer.pop(OboParser::Tokens::TagValuePair)
+        tags.push(t) 
+      rescue
+        raise 
+      end
     end
     @builder.add_typedef(tags)
   end
